9468c7f1a672587914307c076c583d365053819c
Yean <yeeninw55@gmail.com>
2024-05-22 18:53:25 +0700

    first commit

---
+++ src/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_redis import FlaskRedis
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from flask_mail import Mail
+from .config import Config
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+redis_cache = FlaskRedis()
+mail = Mail()
+
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    #init extension
+    db.init_app(app)
+    redis_cache.init_app(app)
+    mail.init_app(app)
+    # create db table
+    from . import models as _
+    with app.app_context():
+        db.create_all()
+    
+    # register blueprint
+    from .views import login , register
+    app.register_blueprint(register.bp)
+    app.register_blueprint(login.bp)
+    
+    for route in app.url_map.iter_rules():
+        print(f"{route.methods} {route}")
+    
+    return app 
---
+++ src/config.py
@@ -0,0 +1,21 @@
+class Config(object):
+
+    # Flask
+    SECRET_KEY = "84b9ebfa0b5bab16df6601da5c35b0f8ebb5d062ebdddd14bff178960dfeea30"
+    SERVER_NAME = "127.0.0.1:5000"
+    # SERVER_NAME = "3ba5-171-103-249-230.ngrok-free.app"
+    APPLICATION_ROOT = "/"
+    PREFERRED_URL_SCHEME = "http"
+    
+    #Flask Redis
+    REDIS_URL = "redis://localhost:6379"
+    
+    #Flask Mail
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USERNAME = "yeeninw55@gmail.com"
+    MAIL_PASSWORD = "gqdg lvgj eygn rpma"
+    MAIL_USE_TLS = True
+    
+    # Flask SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = "sqlite:///laro.db"
